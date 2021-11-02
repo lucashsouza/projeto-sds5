4a0c0882798e603e6d07ba9066212d166e1440d1
lucashsouza <lhenriquesouza00@gmail.com>
2021-11-02 00:29:06 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import Navbar from "components/navbar";
+import Footer from "components/footer";
+import Datatable from "components/datatable";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá, mundo!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Olá, mundo!</h1>
+
+        <Datatable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
