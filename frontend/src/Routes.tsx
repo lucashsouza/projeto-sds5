13c68d25d3bd93a17d847dc7f9f063bb8f65e683
lucashsouza <lhenriquesouza00@gmail.com>
2021-11-06 00:22:42 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,20 @@
+import Home from 'pages/Dashboard';
+import Dashboard from 'pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
